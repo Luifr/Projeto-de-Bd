4833fc23647d978dc9875004e3887a2dce642a7f
luifr <luifr@usp.br>
2019-06-13 19:57:20 +0000

    initial routes

--- controller.js
+++ controller.js
@@ -8,18 +8,93 @@ const pool = new Pool({
 	port: 5432
 });
 
-module.exports.greet = (req,res) => {
-		pool.query("SELECT cpf FROM Funcionarios", (err,data)=>{
+// async function getFuncionarios(){
+// 	let response = await fetch('http://localhost:3000/funcionarios',{
+// 		method: 'GET',
+// 		mode: 'cors'
+// 	});
+// 	let data = await response.json();
+// 	console.log(data.rows);
+// 	let ul = document.getElementById("funcionarioList");
+// 	for(row of data.rows){
+// 		let node = document.createElement('li');
+// 		node.innerText = row.cpf;
+// 		ul.appendChild(node);
+// 	}
+// }
+
+module.exports.getEmployees = (req,res) => {
+		pool.query("SELECT * FROM FUNCIONARIO", (err,data)=>{
 		if(!err){
-			res.send(data);
+			res.send(data.rows);
 		}
 	})
 }
 
-// mas pelo que eu lembre são: buscar os funcionários por cpf/nome (acho que dá pra fazer por cargo tbm)
-// buscar notícias por nome/data
-// buscar episódio por data
-// buscar propaganda por nome/cpnj da empresa
-// daí pros funcionários, dá pra clicar entre os que apareceram na busca pra ver todas as informações deles, e as notícias que ele já participou
-// nas notícias, a msm coisa, mas mostrando as informações e quais funcionários trabalharam nela
-// nos episódios, mostrar os blocos e quais as filmagens editadas de cada um
+module.exports.getEmployeesByCpf = (req,res) => {
+	pool.query(`SELECT * FROM FUNCIONARIO WHERE CPF=${req.params.cpf}`, (err,data)=>{
+		if(!err){
+			res.send(data.rows);
+		}
+	})
+}
+
+module.exports.getEmployeesByName = (req,res) => {
+	pool.query(`SELECT * FROM FUNCIONARIO WHERE NOME=${req.params.name}`, (err,data)=>{
+		if(!err){
+			res.send(data.rows);
+		}
+	})
+}
+
+module.exports.getEmployeesByPosition = (req,res) => {
+	pool.query(`SELECT * FROM FUNCIONARIO WHERE CARGO=${req.params.position}`, (err,data)=>{
+		if(!err){
+			res.send(data.rows);
+		}
+	})
+}
+
+//////////////////////////////////////////////////////////////////////
+
+module.exports.getNews = (req,res) => {
+	pool.query(`SELECT * FROM NOTICIA`, (err,data)=>{
+		if(!err){
+			res.send(data.rows);
+		}
+	})
+}
+
+module.exports.getNewsByName = (req,res) => {
+	pool.query(`SELECT * FROM NOTICIA WHERE TITULO=${req.params.name}`, (err,data)=>{
+		if(!err){
+			res.send(data.rows);
+		}
+	})
+}
+
+module.exports.getNewsByDate = (req,res) => {
+	pool.query(`SELECT * FROM NOTICIA WHERE DATAACONT=${req.params.date}`, (err,data)=>{
+		if(!err){
+			res.send(data.rows);
+		}
+	})
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+module.exports.getEpisodes = (req,res) => {
+	pool.query(`SELECT * FROM EPISODIO`, (err,data)=>{
+		if(!err){
+			res.send(data.rows);
+		}
+	})
+}
+
+module.exports.getEpisodesByDate = (req,res) => {
+	pool.query(`SELECT * FROM EPISODIO WHERE DATA=${req.params.date}`, (err,data)=>{
+		if(!err){
+			res.send(data.rows);
+		}
+	})
+}
--- routes.js
+++ routes.js
@@ -5,4 +5,24 @@ module.exports = router;
 
 const apiController = require('./controller')
 
-router.get('/', apiController.greet);
+router.get('/employees', apiController.getEmployees);
+
+router.get('/employees/:cpf', apiController.getEmployeesByCpf);
+
+router.get('/employees/:name', apiController.getEmployeesByName);
+
+router.get('/employees/:position', apiController.getEmployeesByPosition);
+
+///////////////////////////////////////////////////////////
+
+router.get('/news', apiController.getNews);
+
+router.get('/news/:name', apiController.getNewsByName);
+
+router.get('/news/:date', apiController.getNewsByDate);
+
+///////////////////////////////////////////////////////////
+
+router.get('/episodes', apiController.getEpisodes);
+
+router.get('/episodes/:date', apiController.getEpisodesByDate);
